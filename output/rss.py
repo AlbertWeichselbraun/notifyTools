e54c65f597cc1edf299b0376f3da6fecbe18b83b
albert <albert@52f8a927-27e1-4e1c-b33e-807c988f9568>
2009-10-03 19:31:11 +0000

    temporary version

---
+++ output/rss.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+""" @package notifytools.rss """
+
+# (C)opyrights 2009 by Albert Weichselbraun <albert@weichselbraun.net>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = "$Header$"
+
+
+from datetime import datetime
+import PyRSS2Gen
+
+class RSS(object):
+    """ an RSS output object """
+
+    __slots__ = ('feed_title', 'feed_description', 'notifications' )
+
+    def __init__(self, title, url, description):
+        """ @param[in] feed title
+            @param[in] feed description
+        """
+        self.notifications    = []
+        self.feed_title       = title
+        self.feed_description = description
+
+    
+    def addNotification(title, link, description, date=datetime.now()):
+        """ @param[in] title The entry's title
+            @param[in] link  A link to the entry
+            @param[in] description
+            @param[in] date
+        """
+        self.items.append(
+              PyRSS2Gen.RSSItem(
+                title = title,
+                link = link,
+                description = description , 
+                pubDate = date,
+              )
+           )
+
+    def notify(self, fname, url):
+        """ publishs the rss feed at the given url
+            @param[in] fname
+            @param[in] url
+        """
+        rss = PyRSS2Gen.RSS2(
+            title         = self.feed_title,
+            link          = url,
+            description   = self.feed_description,
+            lastBuildDate = datetime.now(),
+            items         = self.notifications,)
+
+        rss.write_xml(open(fname, "w"))
+        self.notifications = []
+
---
+++ watcher/webwatcher.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+""" Webwatcher """
+
+# (C)opyrights 2009 by Albert Weichselbraun <albert@weichselbraun.net>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__version__ = "$Header$"
+
+from hashlib import sha1
+from commands import getoutput
+from cPickle import load, dump
+import os.path, os
+
+LYNX="/usr/bin/lynx -force_html -nocolor -dump -nolist -nobold -pseudo_inlines=0 -display_charset=utf8 %s"
+STORAGE_PATH=".notify-webwatcher/%s"
+
+class Webwatcher:
+
+    def getChange( self, url ):
+        """ returns the percentage to which this url has changed
+            @param[in] url
+            @returns change (in %)
+        """
+        old = self._loadHashSet( url )
+        new = self._getPageHashSet( url )
+        self._saveHashSet( url, new )
+
+        return float( len(new.difference( old )) )/len(new)
+
+    @staticmethod
+    def getStorageFname( url ):
+        """ returns the storage location for the given url 
+            @param[in] url
+            @returns fname 
+        """
+        return STORAGE_PATH % ( sha1(url).hexdigest() )
+
+    @staticmethod
+    def _saveHashSet( url, hashSet ):
+        """ saves the saved hashSet for the given url 
+            @param[in] url
+            @param[in] hashSet
+        """
+        fname = Webwatcher.getStorageFname( url )
+        if not os.path.exists( os.path.dirname(fname) ):
+            os.makedirs( os.path.dirname(fname) )
+        dump( hashSet, open(fname, "w") )
+ 
+    @staticmethod
+    def _loadHashSet( url ):
+        """ returns the saved hashSet for the given url 
+            @param[in] url
+            @returns a set of hashs describing the url
+        """
+        fname = Webwatcher.getStorageFname( url )
+        if os.path.exists(fname):
+            return load( open(fname) )
+        else:
+            return set()
+    
+    @staticmethod
+    def _getPageHashSet( url ):
+        """ returns the hashSet for the given page 
+            @param[in] url
+            @returns a set of hashs describing one line of the page each 
+        """
+
+        assert "http" in url or "https" in url
+        hashSet = set()
+        for line in getoutput(LYNX % url).split("\n"):
+            hashSet.add( sha1(line).hexdigest() )
+
+        return hashSet
+
+
+class TestWebWatcher(object):
+    """ tests the Webwatcher object """
+
+    def testGetPageHashSet(self):
+        hS = Webwatcher._getPageHashSet( "http://www.heise.de" )
+        print hS
+        asdf
+
+
+if __name__ == '__main__':
+    print Webwatcher().getChange("http://www.heise.de")
